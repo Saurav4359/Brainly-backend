9ec02480515d576f133ebf8b84e564cca44ca03d
Saurav4359 <isauravsharmaokay4359@gmail.com>
2026-02-21 18:49:16 +0530

    cors issue fix

--- src/index.ts
+++ src/index.ts
@@ -9,7 +9,13 @@ import { random } from "./utils/utils.js";
 const jwt_key = "rwqegwerg54";
 const app = express();
 app.use(express.json());
-app.use(cors());
+ 
+
+app.use(cors({
+  origin: "*",   
+  methods: "GET,POST,PUT,DELETE",
+  credentials: true
+}));
 const PORT = process.env.PORT! || 4000;
 
 app.get("/", (req, res) => {
